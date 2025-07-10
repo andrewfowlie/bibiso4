6f0082fa4758cf275f91346bd1681a9923cb04bf
andrewfowlie <andrew.j.fowlie@gmail.com>
2025-07-10 15:39:02 +0800

    bug fixes

--- src/bibiso4/__init__.py
+++ src/bibiso4/__init__.py
@@ -9,7 +9,7 @@ import re
 import sys
 
 
-RM_IGNORE = ["part", "series", "section", "a", "an", "the", "from", "of", "and", "or", "on"]
+RM_IGNORE = ["part", "series", "section", "a", "an", "the", "from", "of", "and", "or", "on", "in"]
 ALLOW_TRAILING = ["A"]
 FIELDS = ["journal", "series"]
 
@@ -26,33 +26,20 @@ with open(os.path.join(DATA, 'suffix.json'), 'r') as handle:
 
 
 def lookup_exact(token):
-
     lower = token.lower()
-
-    abbrv = EXACT.get(lower)
-
-    if abbrv is None:
-        return None
-
-    if not abbrv:
-        return token
-
-    return abbrv
+    return EXACT.get(lower)
 
 
 def lookup_prefix(token):
 
     lower = token.lower()
 
-    for i in range(1, len(lower)):
+    for i in reversed(range(1, len(lower))):
 
-        abbrv = PREFIX.get(lower[:i + 1])
+        substr = lower[:i + 1]
+        abbrv = PREFIX.get(substr)
 
         if abbrv is not None:
-
-            if not abbrv:
-                return token
-
             return abbrv
 
 
@@ -60,15 +47,12 @@ def lookup_suffix(token):
 
     lower = token.lower()
 
-    for i in range(len(lower)):
+    for i in reversed(range(len(lower))):
 
-        abbrv = SUFFIX.get(lower[-i:])
+        substr = lower[-i:]
+        abbrv = SUFFIX.get(substr)
 
         if abbrv is not None:
-
-            if not abbrv:
-                return token
-
             return lower[:len(lower) - len(abbrv)] + abbrv
 
 
